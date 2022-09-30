28171fd2266299ec37a5aaf9e9fa4ebf5c7453bb
Babatunde Okediran <btundeokediran@gmail.com>
2022-09-30 13:14:56 +0100

    Added few changes Project/PressRelease

--- RensourceAPI/Migrations/20220930121047_newMigrations.Designer.cs
+++ RensourceAPI/Migrations/20220930121047_newMigrations.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RensourcePersistence.AppDBContext;
 
@@ -11,9 +12,10 @@ using RensourcePersistence.AppDBContext;
 namespace RensourceAPI.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20220930121047_newMigrations")]
+    partial class newMigrations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RensourceAPI/Migrations/20220930121047_newMigrations.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RensourceAPI.Migrations
+{
+    public partial class newMigrations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ExecutiveTeam_ExecutiveTeamCategory_ExecutiveTeamCategoryId",
+                table: "ExecutiveTeam");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SinglePageTitle",
+                table: "Projects",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "PressRelease",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ExecutiveTeamCategoryId",
+                table: "ExecutiveTeam",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ExecutiveTeam_ExecutiveTeamCategory_ExecutiveTeamCategoryId",
+                table: "ExecutiveTeam",
+                column: "ExecutiveTeamCategoryId",
+                principalTable: "ExecutiveTeamCategory",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ExecutiveTeam_ExecutiveTeamCategory_ExecutiveTeamCategoryId",
+                table: "ExecutiveTeam");
+
+            migrationBuilder.DropColumn(
+                name: "SinglePageTitle",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "PressRelease");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ExecutiveTeamCategoryId",
+                table: "ExecutiveTeam",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ExecutiveTeam_ExecutiveTeamCategory_ExecutiveTeamCategoryId",
+                table: "ExecutiveTeam",
+                column: "ExecutiveTeamCategoryId",
+                principalTable: "ExecutiveTeamCategory",
+                principalColumn: "Id");
+        }
+    }
+}
